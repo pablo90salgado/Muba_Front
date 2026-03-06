d21004163269b423e0f616a10156dc3c663d3f1b
PABLOsalg <pablo90salgado@gmail.com>
2026-03-06 10:39:35 -0300

    fix: corregir error tipográfico en el título de gestión de camas

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import BedCard from "./components/BedCard";
 function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-4 bg-gray-100">
-      <h1 className="text-2xl font-bold mb-4">Gestión de1 Camas</h1>
+      <h1 className="text-2xl font-bold mb-4">Gestión de Camas</h1>
       <BedCard estado="libre" numero={1} />
       <BedCard estado="ocupada" numero={2} />
       <BedCard estado="bloqueada" numero={3} />
